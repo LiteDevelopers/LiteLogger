a0f13fa65bead7d8d6efe28f6b7d533db759be9e
Rollczi <ndejlich5@gmail.com>
2021-10-13 02:12:27 +0200

    Add backend module.

---
+++ litelogger-backend/src/main/java/dev/rollczi/litelogger/LiteLoggerApplication.java
@@ -0,0 +1,13 @@
+package dev.rollczi.litelogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteLoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiteLoggerApplication.class, args);
+    }
+
+}
---
+++ litelogger-backend/src/test/java/dev/rollczi/litelogger/LiteLoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rollczi.litelogger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteLoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
